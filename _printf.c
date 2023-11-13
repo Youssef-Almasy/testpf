3f1562db16fbd7282cff14b95117406c3e9b89ce
youssef-hassane <youssef_hassane@icloud.com>
2023-11-13 20:08:46 +0200

    Adding all the files

--- _printf.c
+++ _printf.c
@@ -12,55 +12,56 @@
 #include "Specifier/Handling_Integer_Specifier.c"
 #include "Specifier/Handling_Percent_Sign_Specifier.c"
 
-
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * _printf - Custom implementation of the printf function.
+ * Formats and prints data according to the provided format string.
+ * Return: The number of characters printed (excluding null terminator).
+ * ----------------------
+ * Prototype:
+ * int _printf(const char *format, ...);
+ * ----------------------
+ * @format: The format string containing format specifiers.
+ * ----------------------
+ * By Youssef Hassane
  */
 int _printf(const char *format, ...)
 {
+	/* Index for iterating through the format string */
 	unsigned int index = 0;
+	/* Length of the formatted string */
 	unsigned int length = 0;
-	/* theIndexOfTheBuffer is used as an index to keep track of */
-	/* the current position in the buffer (theBuff) where */
-	/* characters are being stored for eventual printing. */
+	/* Index in the buffer where characters are stored for printing */
 	unsigned int theIndexOfTheBuffer = 0;
-	/* theBuff is a dynamically allocated buffer that is used to store the */
-	/* characters that are being formatted and will be printed eventually. */
+	/* Dynamically allocated buffer to store formatted characters */
 	char *theBuffer;
-/* */
+	/* Variable argument list */
 	va_list theArgs;
-
-	/* int (*function)(va_list, char *, unsigned int); */
-
+	/* Start the variable argument processing */
 	va_start(theArgs, format);
-/* */
+	/* Allocate memory for the buffer */
 	theBuffer = malloc(sizeof(char) * BUFFERSIZE);
-/* */
+	/* Validate the input format */
 	if (Input_Validation_Condition(format, index))
 	{
-		/* */
+		/* Return failure if input validation fails */
 		return (FAIL);
 	}
-/* */
+	/* Check for an empty format string */
 	if (!format[index])
 	{
-		/* */
+		/* Return success if the format string is empty */
 		return (SUCCESS);
 	}
-/* */
+	/* Process the format string and store the result in the buffer */
 	length = Format_Specifier_Processing_Loop(
 	    format, theBuffer, &theIndexOfTheBuffer,
 	    &length, theArgs);
-/* */
+	/* Display the content of the buffer */
 	Display_The_Buffer(theBuffer, theIndexOfTheBuffer);
-	/* */
+	/* Free the allocated memory for the buffer */
 	free(theBuffer);
-	/* */
+	/* End variable argument processing */
 	va_end(theArgs);
-
+	/* Return the number of characters printed */
 	return (length);
 }
-
