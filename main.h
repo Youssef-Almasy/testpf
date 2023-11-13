4a52c4ef9639a1fcdc8ea80a707c587238a37246
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-13 20:11:24 +0200

    replaced all spaces in main.h file with tabs

--- main.h
+++ main.h
@@ -15,20 +15,20 @@
 int _printf(const char *format, ...);
 
 int Format_Specifier_Processing_Loop(
-    const char *format, char *theBuffer,
-    unsigned int *theIndexOfTheBuffer,
-    unsigned int *length, va_list theArgs);
+	const char *format, char *theBuffer,
+	unsigned int *theIndexOfTheBuffer,
+	unsigned int *length, va_list theArgs);
 
 void Process_Print_Function(
-    const char *format, char *theBuffer,
-    unsigned int *theIndexOfTheBuffer,
-    unsigned int *length, va_list theArgs, unsigned int index);
+	const char *format, char *theBuffer,
+	unsigned int *theIndexOfTheBuffer,
+	unsigned int *length, va_list theArgs, unsigned int index);
 
 int Display_The_Buffer(char *theBuffer, unsigned int numberOfBytesToPrint);
 int Handling_Character_Specifier(
-    va_list theArgs,
-    char *theBuffer,
-    unsigned int theIndexOfTheBuffer);
+	va_list theArgs,
+	char *theBuffer,
+	unsigned int theIndexOfTheBuffer);
 
 int Input_Validation_Condition(const char *theInputString, unsigned int index);
 
@@ -47,38 +47,38 @@ typedef struct print
 } print_t;
 
 unsigned int Combines_Buffer(
-    char *theBuffer,
-    char character,
-    unsigned int numberOfBytesToPrint);
+	char *theBuffer,
+	char character,
+	unsigned int numberOfBytesToPrint);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 
 int Handling_String_Specifier(
-    va_list theArgs,
-    char *theBuffer,
-    unsigned int numberOfBytesToPrint);
+	va_list theArgs,
+	char *theBuffer,
+	unsigned int numberOfBytesToPrint);
 
 int Handling_Integer_Specifier(
-    va_list theArgs,
-    char *theBuffer,
-    unsigned int numberOfBytesToPrint);
+	va_list theArgs,
+	char *theBuffer,
+	unsigned int numberOfBytesToPrint);
 
 int Handle_Integer_Sign(
-    int theInputOfInteger,
-    char *theBuffer,
-    unsigned int *numberOfBytesToPrint);
+	int theInputOfInteger,
+	char *theBuffer,
+	unsigned int *numberOfBytesToPrint);
 
 void Append_Digits_To_Buffer(
-    unsigned int value,
-    unsigned int *numberOfBytesToPrint,
-    char *theBuffer,
-    unsigned int *index);
+	unsigned int value,
+	unsigned int *numberOfBytesToPrint,
+	char *theBuffer,
+	unsigned int *index);
 
 int Handling_Percent_Sign_Specifier(va_list a, char *buffer, unsigned int i);
 
 int Find_Matching_Function_Index(
-    const char *identifier, int position,
-    int *index_1, int *index_2,
-    const print_t *functions);
+	const char *identifier, int position,
+	int *index_1, int *index_2,
+	const print_t *functions);
 
 #endif
