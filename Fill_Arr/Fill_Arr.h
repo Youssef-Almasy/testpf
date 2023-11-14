ac33913f130624ca4b479f4c43480eac166d3a31
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-14 23:22:01 +0200

    Changed Fill_arr.h to Generate_Representation.h

--- Fill_Arr/Fill_Arr.h
+++
@@ -1,3 +0,0 @@
-#include "../main.h"
-
-char *Hex_arr_fill(char *bnr, char *hexadecimal, int isUppercase, int limit);
---
+++ Fill_Arr/Generate_Representation.h
@@ -0,0 +1,6 @@
+#include "../main.h"
+
+char *Hex_arr_fill(char *bnr, char *hexadecimal, int isUppercase, int limit);
+char *Generate_Binary_Representation(
+	char *binaryRepresentation, long int theGivenNumber,
+	int isNegative, int theLimitOfBinary);
