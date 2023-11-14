2b7194138ba8e629559857751dcec8d6b25c964f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-14 23:35:01 +0200

    Remove white lines

--- Generate_Representation/Generate_Representation.h
+++ Generate_Representation/Generate_Representation.h
@@ -1,6 +1,11 @@
 #include "../main.h"
 
-char *Generate_Hex_Representation(char *bnr, char *hexadecimal, int isUppercase, int limit);
+char *Generate_Hex_Representation(
+		char *bnr,
+		char *hexadecimal,
+		int isUppercase,
+		int limit);
+
 char *Generate_Binary_Representation(
 	char *binaryRepresentation, long int theGivenNumber,
 	int isNegative, int theLimitOfBinary);
